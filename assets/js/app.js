4a1cd0b833c8d02963affa0fa248c6e8176e7de0
Leandro Muzzupappa <leann.muzzu@gmail.com>
2019-12-09 12:27:29 -0300

    Desktop almost done

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.testimonials_slider').slick({
+        arrows: true,
+        prevArrow: $('.arrow_prev'),
+        nextArrow: $('.arrow_next'),
+        autoplay: true,
+    })    
+})
