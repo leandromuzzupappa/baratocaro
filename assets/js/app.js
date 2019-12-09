f0897d83a10934a5bce4e39519a7b37eea9adfd4
Leandro Muzzupappa <leann.muzzu@gmail.com>
2019-12-09 16:01:16 -0300

    Update n

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,28 @@
+const overlay = $('.overlay');
+const preLoader = $('.preloader');
+const html = $('html');
+const phoneImg = $('.phone');
+
+function pageLoaded () {
+    setTimeout(() => {
+        overlay.fadeOut('slow');
+        preLoader.fadeOut('slow');
+        html.css('overflow', 'auto');
+    }, 500);
+}
+
+
+
 $(document).ready(function () {
+    // Inicializo el slider
     $('.testimonials_slider').slick({
         arrows: true,
         prevArrow: $('.arrow_prev'),
         nextArrow: $('.arrow_next'),
         autoplay: true,
-    })    
+    }) 
+
+    // Cierro el preloader cuando carga la pagina
+    pageLoaded();
+
 })
